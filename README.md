7322f7c2627b7192ba7d8f4dead5cbcb2132f2d3
gobuyun <874751353@qq.com>
2019-11-11 15:34:25 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Seeed IRCamera
+
+The IRCamera is a 32x24 thermal imagery sensor.
+
+## Installing from PyPI
+
+On supported GNU/Linux systems like the Raspberry Pi, you can install the driver locally from PyPI. To install for current user:
+```
+sudo pip3 install seeed_ircamera
+```
+On supported Windows systems, you can install the driver locally from PyPI. To install for current user:
+```
+pip3 install seeed_ircamera
+```
+
+## Usage Notes
+
+```python
+import seeed_ircamera
+seeed_ircamera.run()
+```
+
+## Contributing
+If you have any good suggestions or comments, you can send issues or PR us.
---
+++ setup.py
@@ -0,0 +1,65 @@
+# coding=utf-8
+
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='seed_ircamera',
+    version='0.1.0',
+    description='32 x 24 pixels IRCamera Application',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+
+    # The project's main homepage.
+    url='https://github.com/Seeed-Studio',
+
+    # Author details
+    author='gobuyun',
+    author_email='874751353@qq.com',
+
+    install_requires=[
+        'PyQt5',
+        'opencv-python',
+        'numpy',
+        'serial'
+    ],
+
+    # Choose your license
+    license='MIT',
+    packages=find_packages(),
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Hardware',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+
+    # What does your project relate to?
+    keywords='seeed 32x24 IR sensor Application',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    # py_modules=[],
+)
